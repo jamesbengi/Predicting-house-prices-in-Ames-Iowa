36966165e53dde968deaf0ac49935d2d3c1cf39d
james bengi <jamesbengi21@gmail.com>
2023-11-24 16:19:38 +0300

    streamlit app

--- data.py
+++ data.py
@@ -4,5 +4,5 @@ import sklearn.metrics as metrics
 import pickle
 def next():
     data=pd.read_csv('C:/Users/User/Desktop/Data Analytics projects dataset/houses prediction/train.csv')
-
+    st.write('Data Frame')
     st.dataframe(data.head(5))
--- predict.py
+++ predict.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pickle
 import numpy as np
-import sklearn.metrics as metrics
 
 def load_model():
     with open('model.pk','rb') as file:
