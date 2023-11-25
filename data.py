b795b41142471641831df74dbfa746290c68611a
james bengi <jamesbengi21@gmail.com>
2023-11-25 12:36:14 +0300

    train data

--- data.py
+++ data.py
@@ -2,6 +2,6 @@ import streamlit as st
 import pandas as pd
 import pickle
 def next():
-    data=pd.read_csv('C:/Users/User/Desktop/Data Analytics projects dataset/houses prediction/train.csv')
+    data=pd.read_csv('/jamesbengi/Predicting-house-prices-in-Ames-Iowa/blob/master/train.csv')
     st.write('Data Frame')
     st.dataframe(data.head(5))
